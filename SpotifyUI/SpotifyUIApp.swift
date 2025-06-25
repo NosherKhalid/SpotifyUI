838fc5eff4acc13d9f2413597e00a3a68d573b50
Nosher Khalid <nosherkhalid@gmail.com>
2025-06-25 20:11:56 +0500

    Initial Commit

---
+++ SpotifyUI/SpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUIApp.swift
+//  SpotifyUI
+//
+//  Created by Nosher Khalid on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
